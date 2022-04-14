d2b8cc61ce0a21e001a5ebaf2e1a5fe1b5d9f7cc
Ohno <51406176+Ohno-Tk@users.noreply.github.com>
2022-04-14 23:14:46 +0900

    [add]カウンター実装

---
+++ If-you-shout-its-over/Assets/Scripts/Counter.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour
+{
+    private Text text;
+    private int NowCount = 0; // 現在のカウント
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = this.GetComponent<Text> ();
+
+        TextIntToString();
+    }
+
+    // カウントの初期化
+    public void InitializeCount()
+    {
+        NowCount = 0;
+
+        TextIntToString();
+    }
+
+    // カウントアップ
+    public void CountUp()
+    {
+        NowCount++;
+
+        TextIntToString();
+    }
+
+    // カウントダウン
+    public void CountDown()
+    {
+        NowCount--;
+
+        TextIntToString();
+    }
+
+    // intを2桁表示にしてstringに変換
+    private void TextIntToString()
+    {
+        text.text = NowCount.ToString("00");
+    }
+}
