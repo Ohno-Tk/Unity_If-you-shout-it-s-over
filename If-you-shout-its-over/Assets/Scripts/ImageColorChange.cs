ed2f2911cffc678e193bb5eae064d8d0ee360c9d
Ohno <51406176+Ohno-Tk@users.noreply.github.com>
2022-04-24 13:38:25 +0900

    [add]設定ボタンにカーソル当てたらわかりやすいようにした

---
+++ If-you-shout-its-over/Assets/Scripts/ImageColorChange.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageColorChange : MonoBehaviour
+{
+    public void Show()
+    {
+        GetComponent<Image>().color = new Color32 (125, 125, 125, 125);
+    }
+
+    public void Hide()
+    {
+        GetComponent<Image>().color = new Color32 (255, 255, 255, 255);
+    }
+}
